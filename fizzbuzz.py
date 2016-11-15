9ecdd45bf0bda4a2d8bbb0af5fd431ec2829d5b4
Ariadyne Oliveira <ariadyne.oliveira@yahoo.com.br>
2016-11-15 11:12:02 -0200

    Refatoracao do teste

---
+++ fizzbuzz.py
@@ -0,0 +1,58 @@
+"""
+Regras do FizzBuzz
+
+1. Se a posição for múltipla de 3: fizz
+2. Se a posição for múltipla de 5: buzz
+3. Se a posição for múltipla de 3 e 5: fizzbuzz
+4. Para qualquer outra posição fale o próprio nº
+
+"""
+
+from functools import partial
+
+
+multiple_of = lambda base, num: num % base == 0
+multiple_of_5 = partial(multiple_of, 5)
+multiple_of_3 = partial(multiple_of, 3)
+
+
+"""
+def robot(pos):
+    say = str(pos)
+    if multiple_of_3(pos) and multiple_of_5(pos):
+        say = 'fizzbuzz'
+    elif multiple_of_5(pos):
+        say = 'buzz'
+    elif multiple_of_3(pos):
+        say= 'fizz'
+
+    return say
+
+def assert_equal(result, expected):
+    from sys import _getframe
+
+    msg = 'Fail: line {} got {} expecting {}'
+
+    if not result == expected:
+       current = _getframe()
+       caller = current.f_back
+       line_no = caller.f_lineno
+       print(msg.format(line_no, result, expected))
+
+if __name__ == '__main__':
+    assert_equal(robot(1), '1')
+    assert_equal(robot(2), '2')
+    assert_equal(robot(4), '4')
+
+    assert_equal(robot(3), 'fizz')
+    assert_equal(robot(6), 'fizz')
+    assert_equal(robot(9), 'fizz')
+
+    assert_equal(robot(5), 'buzz')
+    assert_equal(robot(10), 'buzz')
+    assert_equal(robot(20), 'buzz')
+
+    assert_equal(robot(15), 'fizzbuzz')
+    assert_equal(robot(30), 'fizzbuzz')
+    assert_equal(robot(45), 'fizzbuzz')
+"""
